b56b99563e3457f82069c58e0237dd95d265cfab
zetpom <yel1412.wk@gmail.com>
2022-12-20 16:02:22 +0700

    final

--- CovidSoapClient/src/covidsoapclient/CovidSoapClient.java
+++ CovidSoapClient/src/covidsoapclient/CovidSoapClient.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package covidapp;
+package covidsoapclient;
 
 import services.Covidtables;
 
@@ -11,7 +11,7 @@ import services.Covidtables;
  *
  * @author ZETPOM
  */
-public class CovidApp {
+public class CovidSoapClient {
 
     /**
      * @param args the command line arguments
